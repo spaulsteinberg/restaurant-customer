d10507a90658ca167d486467b0fcbfb4c5d1ce28
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-12-23 14:09:22 -0600

    close #56 cart integrated

--- src/components/cart/CartItemBody.tsx
+++ src/components/cart/CartItemBody.tsx
@@ -1,31 +1,50 @@
 import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { useDispatch } from "react-redux";
-import { DRINK_TYPE } from "../../constants/keys";
+import { addCartItem, removeCartItem } from "../../api";
+import { FOOD_TYPE } from "../../constants/keys";
+import { useSession } from "../../contexts/SessionContext";
 import { decrementCartCount, incrementCartCount, insertBeverage, insertFood, removeBeverage, removeFood } from "../../redux/cart/cartActions";
 import Incrementer from "../utility/Incrementer";
 
 type CartItemBodyProps = {
-    data:any;
+    data: any;
 }
 
-const CartItemBody = ({data}:CartItemBodyProps) => {
-    
+const CartItemBody = ({ data }: CartItemBodyProps) => {
+
     const [quantity, setQuantity] = useState<number>(data.quantity);
     const dispatch = useDispatch();
+    const session = useSession();
+
+    const isFoodItem = data.type === FOOD_TYPE
 
     const processCartAddition = () => {
-        dispatch(incrementCartCount());
-        data.type === DRINK_TYPE ? dispatch(insertBeverage({name: data.item, data: data, quantity: quantity + 1})) : dispatch(insertFood({name: data.item, data: data, quantity: quantity + 1}))
-        setQuantity(q => q + 1);
+        if (!session?.sessionId) return;
+
+        addCartItem(session.sessionId, { item: data.item, category: data.category, price: data.price, quantity: quantity + 1, type: data.type }, false, isFoodItem)
+            .then(res => {
+                dispatch(incrementCartCount());
+                isFoodItem ? dispatch(insertFood({ name: data.item, data: data, quantity: quantity + 1 })) : dispatch(insertBeverage({ name: data.item, data: data, quantity: quantity + 1 }))
+                setQuantity(q => q + 1);
+            })
+            .catch(err => console.log(err))
     }
 
     const processCartSubtraction = () => {
-        dispatch(decrementCartCount());
-        data.type === DRINK_TYPE ? dispatch(removeBeverage({name: data.item, data: data, quantity: quantity - 1})) : dispatch(removeFood({name: data.item, data: data, quantity: quantity - 1}))
-        setQuantity(q => q - 1);
+        if (!session?.sessionId) return;
+        removeCartItem(session.sessionId, { item: data.item, category: data.category, price: data.price, quantity: quantity - 1, type: data.type}, quantity - 1 < 1, isFoodItem)
+        .then(res => {
+            setQuantity(q => q - 1);
+            dispatch(decrementCartCount());
+            isFoodItem ? dispatch(removeFood({ name: data.item, data: data, quantity: quantity - 1 })) : dispatch(removeBeverage({ name: data.item, data: data, quantity: quantity - 1 }))
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
-    const handleChange = (num:number) => {
+
+    const handleChange = (num: number) => {
         num > 0 ? processCartAddition() : processCartSubtraction();
     }
 
